e2ca2b59b46de85d3c3470938e6221f03b9c8b34
Douglas Brochier <douglasbrochier@yahoo.com>
2024-09-05 22:33:24 -0300

    day6

--- desafiopython/paginaweb/models.py
+++ desafiopython/paginaweb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Personagem(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome_original = models.CharField(max_length=255)
+    nome_traduzido = models.CharField(max_length=255)
+    afiliacao_original = models.CharField(max_length=255)
+    afiliacao_traduzida = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome_traduzido  # Isso faz com que o nome traduzido apareça como representação do objeto.
--- desafiopython/paginaweb/views.py
+++ desafiopython/paginaweb/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import requests
 from googletrans import Translator
-
+from .models import Personagem
 
 def traduzir_personagens():
     url = "https://last-airbender-api.fly.dev/api/v1/characters"
@@ -16,9 +16,8 @@ def traduzir_personagens():
     personagens = response.json()
     
     translator = Translator()
-    personagens_traduzidos = []
     
-    # Traduzir e exibir nomes e afiliações
+    # Traduzir nomes e afiliações
     for personagem in personagens:
         nome = personagem.get('name', 'N/A')
         afiliacao = personagem.get('affiliation', 'N/A')
@@ -26,16 +25,16 @@ def traduzir_personagens():
         nome_traduzido = translator.translate(nome, dest='pt').text
         afiliacao_traduzida = translator.translate(afiliacao, dest='pt').text
         
-        personagens_traduzidos.append({
-            'nome_original': nome,
-            'nome_traduzido': nome_traduzido,
-            'afiliacao_original': afiliacao,
-            'afiliacao_traduzida': afiliacao_traduzida,
-        })
-
-    return personagens_traduzidos
+        # Salvando os personagens no banco de dados
+        Personagem.objects.create(
+            nome_original=nome,
+            nome_traduzido=nome_traduzido,
+            afiliacao_original=afiliacao,
+            afiliacao_traduzida=afiliacao_traduzida
+        )
 
 def home(request):
-    personagens_traduzidos = traduzir_personagens()
+    traduzir_personagens() 
+    personagens_traduzidos = Personagem.objects.all()  # Busca todos os personagens salvos
     context = {'personagens': personagens_traduzidos}
     return render(request,'paginainicial\home.html', context)
